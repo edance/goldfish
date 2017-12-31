d3ac6253c0ca8961481d15f586ebf4f668f9119f
Evan Dancer <evancdancer@gmail.com>
2017-12-31 18:48:11 -0500

    ✨ Add presence to notifications

--- lib/goldfish_web/channels/notification_channel.ex
+++ lib/goldfish_web/channels/notification_channel.ex
@@ -2,12 +2,22 @@ defmodule GoldfishWeb.NotificationChannel do
   use GoldfishWeb, :channel
 
   alias Goldfish.Chat
+  alias GoldfishWeb.Presence
 
   def join("notifications", _params, socket) do
     if Guardian.Phoenix.Socket.authenticated?(socket) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       :error
     end
+  end
+
+  def handle_info(:after_join, socket) do
+    push(socket, "presence_state", Presence.list(socket))
+    user = socket.assigns.current_user
+    time = System.system_time(:seconds)
+    {:ok, _} = Presence.track(socket, user.id, %{online_at: inspect(time)})
+    {:noreply, socket}
   end
 end
