3250cee6e40622201c7d01d9daa07b0a5f0b233c
Evan Dancer <evancdancer@gmail.com>
2018-01-01 20:42:52 -0500

    ✨ Add welcome messages

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -16,6 +16,38 @@ chatInput.on('paste', event => {
   document.execCommand('insertText', false, text);
 });
 
+const scrollToBottom = ()=> {
+  messagesContainer.scrollTop(messagesContainer[0].scrollHeight);
+};
+
+const addMessage = (message) => {
+  const className = message.bot ? 'bot' : '';
+  const template =
+        `<div class='clearing'>
+           <div class='message ${className}'>
+             ${message.body}
+             <div class='time'>${message.inserted_at}</div>
+           </div>
+         </div>`;
+
+  let messageItem = document.createElement('div');
+  messageItem.innerHTML = template;
+  messagesContainer.append(messageItem);
+  scrollToBottom();
+};
+
+const sendWelcomeMessages = () => {
+  const messages = ['Hi there!', 'I am Evan\'s Website Bot', 'Ask me anything (about him)?'];
+  messages.map((body, idx) => {
+    const message = {
+      body,
+      bot: true,
+      inserted_at: new Date(),
+    };
+    setTimeout(() => addMessage(message), idx * 1500);
+  });
+};
+
 chatInput.on('keypress', event => {
   if(event.keyCode === 13 && !event.shiftKey){
     event.preventDefault();
@@ -24,26 +56,12 @@ chatInput.on('keypress', event => {
   }
 });
 
-channel.on('new_msg', payload => {
-  const className = payload.bot ? 'bot' : '';
-  const template =
-        `<div class='clearing'>
-           <div class='message ${className}'>
-             ${payload.body}
-             <div class='time'>${payload.inserted_at}</div>
-           </div>
-         </div>`;
-
-  let messageItem = document.createElement('div');
-  messageItem.innerHTML = template;
-  messagesContainer.append(messageItem);
-  scrollToBottom();
-});
-
-const scrollToBottom = ()=> {
-  messagesContainer.scrollTop(messagesContainer[0].scrollHeight);
-};
+channel.on('new_msg', addMessage);
 
 scrollToBottom();
+
+if (!messagesContainer.html().trim()) {
+  sendWelcomeMessages();
+}
 
 channel.join();
