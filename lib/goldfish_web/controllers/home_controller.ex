acef08dd6862847ada1f881a573a90a7c347fad2
Evan Dancer <evancdancer@gmail.com>
2018-01-16 19:58:18 -0500

    ✨ Add links to recent posts

--- lib/goldfish_web/controllers/home_controller.ex
+++ lib/goldfish_web/controllers/home_controller.ex
@@ -2,9 +2,11 @@ defmodule GoldfishWeb.HomeController do
   use GoldfishWeb, :controller
 
   alias Goldfish.Chat
+  alias Goldfish.CMS
 
   def index(conn, _params) do
     messages = Chat.list_messages(conn.assigns.room_id)
-    render(conn, "index.html", messages: messages)
+    pages = CMS.list_pages
+    render(conn, "index.html", messages: messages, pages: pages)
   end
 end
