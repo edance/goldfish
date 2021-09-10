756b1105fc41ee92389bdfe3a047e52f4225dc77
Evan Dancer <evancdancer@gmail.com>
2021-09-09 19:38:59 -0700

    Add page title

--- lib/goldfish_web/views/home_view.ex
+++ lib/goldfish_web/views/home_view.ex
@@ -1,3 +1,7 @@
 defmodule GoldfishWeb.HomeView do
   use GoldfishWeb, :view
+
+  def title(_, _) do
+    "Howdy, I'm Evan"
+  end
 end
--- lib/goldfish_web/views/page_view.ex
+++ lib/goldfish_web/views/page_view.ex
@@ -1,6 +1,10 @@
 defmodule GoldfishWeb.PageView do
   use GoldfishWeb, :view
 
+  def title(_, %{ page: page }) do
+    page.title
+  end
+
   def markdown(body) do
     options = %Earmark.Options{code_class_prefix: "lang- language-"}
     Earmark.as_html!(body, options)
