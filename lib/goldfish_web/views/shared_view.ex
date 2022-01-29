13f5a111ec9edbe2388bdf8788698eace939cbd7
Evan Dancer <evancdancer@gmail.com>
2022-01-28 17:51:47 -0800

    Fix broken fonts

--- lib/goldfish_web/views/shared_view.ex
+++ lib/goldfish_web/views/shared_view.ex
@@ -4,7 +4,7 @@ defmodule GoldfishWeb.SharedView do
   def nav_link(conn, text, opts) do
     opts = Keyword.merge(opts,
       class: "inline-block py-2 px-4 no-underline",
-      class_active: "text-gray-900 font-bold",
+      class_active: "text-gray-900 font-normal",
       class_inactive: "text-gray-600 hover:text-gray-900 hover:text-underline"
     )
     active_link(conn, text, opts)
